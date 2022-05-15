b882a0cf9b52a88fd6914cc4acbac449ba383235
popizhen <84291421+popizhen@users.noreply.github.com>
2022-05-15 18:11:31 +0800

    Update test.js

--- test.js
+++ test.js
@@ -43,7 +43,7 @@
     "dtActorR": "",
     "dtDesc": "//div[contains(text(), '剧情')]/child::span/text()",
     "dtDescR": "",
-    "dtFromNode": "//header/dl/dt[not(contains(text(),'秒播'))]/text()",
+    "dtFromNode": "//dl/dt[not(contains(text(),'秒播'))]",
     "dtFromName": "/text()",
     "dtFromNameR": "",
     "dtUrlNode": "//div[@class='playlist clearfix']/ul",
