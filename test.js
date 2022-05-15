4d9219bb5101405d7cce1d123ceaef52e4570cef
popizhen <84291421+popizhen@users.noreply.github.com>
2022-05-15 12:43:47 +0800

    Update test.js

--- test.js
+++ test.js
@@ -49,7 +49,7 @@
     "dtUrlNode": "//div[@class='playlist clearfix']/ul",
     "dtUrlSubNode": "/li/a",
     "dtUrlId": "/@onclick",
-    "dtUrlIdR": "\\S+\\('(.*)'\\);",
+    "dtUrlIdR": "\\S+\('(.*)'\);",
     "dtUrlName": "/text()",
     "dtUrlNameR": "",
     "playUrl": "{playUrl}",
