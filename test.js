27cc26177794632aa8bede207c0c2f1e651091ec
popizhen <84291421+popizhen@users.noreply.github.com>
2022-05-16 12:52:59 +0800

    Update test.js

--- test.js
+++ test.js
@@ -13,7 +13,7 @@
   "homeVodIdR": "/show(\\S+).html",
   "homeVodImg": "/a/img/@src",
   "homeVodImgR": "",
-  "homeVodMark": "concat(/div/h1/em/text(),' ㅤ',/div/div/a/text())",
+  "homeVodMark": "concat(/div/h1/em/text(),' ♡﻿ㅤ',/div/div/a/text())",
   "homeVodMarkR": "- (\\S+)",
   "cateUrl": "https://v.xuangz.cn/whole/{cateId}_{area}_{class}__{year}___0_{by}_{catePg}.html",
   "cateVodNode": "//div[@class='movie-item-in']",
