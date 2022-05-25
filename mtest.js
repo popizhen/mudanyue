c1f54bdd4f7fb4d8de977c907f2ec2e85d24060f
popizhen <84291421+popizhen@users.noreply.github.com>
2022-05-25 21:15:45 +0800

    Update mtest.js

--- mtest.js
+++ mtest.js
@@ -1,71 +1,291 @@
 {
     "author": "春风",
     "ua": "",
-    "homeUrl": "https://www.llyady.com/",
+    "homeUrl": "http://app.lmvideos.cn/",
     "dcVipFlag": "true",
-    "pCfgJs": "https://www.llyady.com/static/js/playerconfig.js",
+    "pCfgJs": "http://app.lmvideos.cn/static/js/playerconfig.js",
     "pCfgJsR": "MacPlayerConfig.player_list=([\\w\\W]*?),MacPlayerConfig.downer_list=",
     "dcShow2Vip": {},
     "dcPlayUrl": "true",
     "cateManual": {
-        "国产剧": "28",
-        "日剧韩": "29",
-        "欧美剧": "30",
-        "港台剧": "31",
-        "其它剧": "32",
         "电影": "1",
+        "剧集": "2",
         "动漫": "4",
-        "综艺": "3"
+        "综艺": "3",
+        "音乐MV": "32",
+        "4K剧集": "31",
+        "4K电影": "30",
+        "纪录片": "29"
     },
-    "homeVodNode": "//div[@class='module-item']",
-    "homeVodName": "//div[@class='module-item-pic']/a/@title",
-    "homeVodId": "//div[@class='module-item-pic']/a/@href",
-    "homeVodIdR": "/video/(\\w+).html",
-    "homeVodImg": "//div[@class='module-item-pic']/img/@data-src",
+    "homeVodNode": "//a[@class='stui-vodlist__thumb lazyload' or @class='stui-vodlist__thumb active lazyload' and not(contains(@href,'topic'))]",
+    "homeVodName": "/@title",
+    "homeVodId": "/@href",
+    "homeVodIdR": "/vod/play/(\\S+).html",
+    "homeVodImg": "/@data-original",
     "homeVodImgR": "",
-    "homeVodMark": "//div[@class='module-item-text']/text()",
-    "cateUrl": "https://www.llyady.com/vodshow/{cateId}--------{catePg}---.html",
-    "cateVodNode": "//div[@class='module-item']",
-    "cateVodName": "//div[@class='module-item-pic']/a/@title",
-    "cateVodId": "//div[@class='module-item-pic']/a/@href",
-    "cateVodIdR": "/video/(\\w+).html",
-    "cateVodImg": "//div[@class='module-item-pic']/img/@data-src",
+    "homeVodMark": "/span[contains(@class,'pic-text text-right') or contains(@class,'picc-text')]/text()",
+//    "cateUrl": "http://app.lmvideos.cn/vod/type/id/{cateId}/page/{catePg}.html",
+    "cateUrl": "http://app.lmvideos.cn/vod/show/area/{area}/by/{by}/id/{cateId}/page/{catePg}/year/{year}.html",
+    "cateVodNode": "//a[@class='stui-vodlist__thumb lazyload']",
+    "cateVodName": "/@title",
+    "cateVodId": "/@href",
+    "cateVodIdR": "/vod/play/(\\S+).html",
+    "cateVodImg": "/@data-original",
     "cateVodImgR": "",
-    "cateVodMark": "//div[@class='module-item-text']/text()",
-    "dtUrl": "https://www.llyady.com/video/{vid}.html",
+    "cateVodMark": "/span[@class='pic-text text-right']/text()",
+    "dtUrl": "http://app.lmvideos.cn/vod/play/{vid}.html",
     "dtNode": "//body",
-    "dtName": "//h1[@class='page-title']/text()",
+    "dtName": "//li[@class='title']/text()",
     "dtNameR": "",
-    "dtImg": "//div[@class='video-cover']//img/@data-src",
+    "dtImg": "//a[@class='stui-vodlist__thumb lazyload']/@data-original",
     "dtImgR": "",
-    "dtCate": "//span[contains(text(), '/')]/following-sibling::a[2]/text()",
+    "dtCate": "",
     "dtCateR": "",
-    "dtYear": "//a[2][@class='tag-link']/text()",
+    "dtYear": "",
     "dtYearR": "",
-    "dtArea": "//a[3][@class='tag-link']/text()",
+    "dtArea": "",
     "dtAreaR": "",
-    "dtDirector": "//span[contains(text(), '导演')]/following-sibling::div/a/text()",
+    "dtDirector": "//span[contains(text(), '导演')]/following-sibling::a/text()",
     "dtDirectorR": "",
-    "dtActor": "//span[contains(text(), '主演')]/following-sibling::div/a/text()",
+    "dtActor": "//span[contains(text(), '主演')]/following-sibling::a/text()",
     "dtActorR": "",
-    "dtDesc": "//span[contains(text(), '剧情')]/following-sibling::div/span/text()",
+    "dtDesc": "//font[contains(text(), '当前更新')]/parent::text()",
     "dtDescR": "",
-    "dtFromNode": "//div[@class='module-tab-item tab-item']/span",
+    "dtFromNode": "//ul[contains(@class,'play-tab')]/li/a",
     "dtFromName": "/text()",
     "dtFromNameR": "",
-    "dtUrlNode": "//div[@class='sort-item']",
-    "dtUrlSubNode": "/a",
+    "dtUrlNode": "//ul[contains(@class,'stui-play__list')]",
+    "dtUrlSubNode": "/li/a",
     "dtUrlId": "@href",
-    "dtUrlIdR": "/player/(\\S+).html",
-    "dtUrlName": "/span/text()",
+    "dtUrlIdR": "/vod/play/(\\S+).html",
+    "dtUrlName": "/text()",
     "dtUrlNameR": "",
-    "playUrl": "https://www.llyady.com/player/{playUrl}.html",
-    "playUa": "Mozilla/5.0 (Linux; Android 10; LYA-AL00; HMSCore 6.5.0.312; GMSCore 20.15.16) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 HuaweiBrowser/11.0.4.300 Mobile Safari/537.36",
-    "searchUrl": "https://www.llyady.com/vodsearch/-------------.html?wd={wd}",
-    "scVodNode": "//div[@class='module-search-item']",
-    "scVodName": "//div[@class='video-info-header']/a/@title",
-    "scVodId": "//div[@class='video-info-header']/a/@href",
-    "scVodIdR": "/video/(\\w+).html",
-    "scVodImg": "//img/@data-src",
-    "scVodMark": "//div[@class='video-info-header']/a/text()"
+    "playUrl": "http://app.lmvideos.cn/vod/play/{playUrl}.html",
+    "playUa": "",
+    "searchUrl": "http://app.lmvideos.cn/vod/search.html?wd={wd}&submit=",
+    "scVodNode": "//a[@class='stui-vodlist__thumb lazyload']",
+    "scVodName": "/@title",
+    "scVodId": "/@href",
+    "scVodIdR": "/vod/play/(\\S+).html",
+    "scVodImg": "/@data-original",
+    "scVodMark": "/span[@class='pic-text text-right']/text()",
+   "filter":{
+"1": [
+{
+"name":"类型",
+"key":"cateId",
+"value":[
+{"n":"全部","v":"1"}
+]
+},
+{
+"name":"年份",
+"key":"year",
+"value":[
+{"n":"全部","v":""},
+{"n":"2022","v":"2022"},
+{"n":"2021","v":"2021"},
+{"n":"2020","v":"2020"},
+{"n":"2019","v":"2019"},
+{"n":"2018","v":"2018"},
+{"n":"2017","v":"2017"},
+{"n":"2016","v":"2016"},
+{"n":"2015","v":"2015"},
+{"n":"2014","v":"2014"},
+{"n":"2013","v":"2013"},
+{"n":"2012","v":"2012"},
+{"n":"2011","v":"2011"},
+{"n":"2010","v":"2010"}
+]
+},
+{
+"name":"地区",
+"key":"area",
+"value":[
+{"n":"全部","v":""},
+{"n":"大陆","v":"大陆"},
+{"n":"香港","v":"香港"},
+{"n":"台湾","v":"台湾"},
+{"n":"美国","v":"美国"},
+{"n":"韩国","v":"韩国"},
+{"n":"日本","v":"日本"},
+{"n":"英国","v":"英国"},
+{"n":"法国","v":"法国"},
+{"n":"泰国","v":"泰国"},
+{"n":"西班牙","v":"西班牙"},
+{"n":"德国","v":"德国"},
+{"n":"意大利","v":"意大利"},
+{"n":"其它","v":"其它"}
+]
+},
+{
+"name":"排序",
+"key":"by",
+"value":[
+{"n":"全部","v":""},
+{"n":"时间","v":"time"},
+{"n":"人气","v":"hits"},
+{"n":"评分","v":"score"}
+]
+}
+],
+"2": [
+{
+"name":"类型",
+"key":"cateId",
+"value":[
+{"n":"全部","v":"2"}
+]
+},
+{
+"name":"年份",
+"key":"year",
+"value":[
+{"n":"全部","v":""},
+{"n":"2022","v":"2022"},
+{"n":"2021","v":"2021"},
+{"n":"2020","v":"2020"},
+{"n":"2019","v":"2019"},
+{"n":"2018","v":"2018"},
+{"n":"2017","v":"2017"},
+{"n":"2016","v":"2016"},
+{"n":"2015","v":"2015"},
+{"n":"2014","v":"2014"},
+{"n":"2013","v":"2013"},
+{"n":"2012","v":"2012"},
+{"n":"2011","v":"2011"},
+{"n":"2010","v":"2010"}
+]
+},
+{
+"name":"地区",
+"key":"area",
+"value":[
+{"n":"全部","v":""},
+{"n":"大陆","v":"大陆"},
+{"n":"香港","v":"香港"},
+{"n":"台湾","v":"台湾"},
+{"n":"美国","v":"美国"},
+{"n":"韩国","v":"韩国"},
+{"n":"日本","v":"日本"},
+{"n":"泰国","v":"泰国"},
+{"n":"新加坡","v":"新加坡"},
+{"n":"英国","v":"英国"},
+{"n":"其他","v":"其他"}
+]
+},
+{
+"name":"排序",
+"key":"by",
+"value":[
+{"n":"全部","v":""},
+{"n":"时间","v":"time"},
+{"n":"人气","v":"hits"},
+{"n":"评分","v":"score"}
+]
+}
+],
+"3": [
+{
+"name":"类型",
+"key":"cateId",
+"value":[
+{"n":"全部","v":"3"}
+]
+},
+{
+"name":"年份",
+"key":"year",
+"value":[
+{"n":"全部","v":""},
+{"n":"2022","v":"2022"},
+{"n":"2021","v":"2021"},
+{"n":"2020","v":"2020"},
+{"n":"2019","v":"2019"},
+{"n":"2018","v":"2018"},
+{"n":"2017","v":"2017"},
+{"n":"2016","v":"2016"},
+{"n":"2015","v":"2015"},
+{"n":"2014","v":"2014"},
+{"n":"2013","v":"2013"},
+{"n":"2012","v":"2012"},
+{"n":"2011","v":"2011"},
+{"n":"2010","v":"2010"}
+]
+},
+{
+"name":"地区",
+"key":"area",
+"value":[
+{"n":"全部","v":""},
+{"n":"大陆","v":"大陆"},
+{"n":"香港","v":"香港"},
+{"n":"台湾","v":"台湾"},
+{"n":"美国","v":"美国"},
+{"n":"韩国","v":"韩国"},
+{"n":"其它","v":"其它"}
+]
+},
+{
+"name":"排序",
+"key":"by",
+"value":[
+{"n":"全部","v":""},
+{"n":"时间","v":"time"},
+{"n":"人气","v":"hits"},
+{"n":"评分","v":"score"}
+]
+}
+],
+"4": [
+{
+"name":"类型",
+"key":"cateId",
+"value":[
+{"n":"全部","v":"4"}
+]
+},
+{
+"name":"年份",
+"key":"year",
+"value":[
+{"n":"全部","v":""},
+{"n":"2022","v":"2022"},
+{"n":"2021","v":"2021"},
+{"n":"2020","v":"2020"},
+{"n":"2019","v":"2019"},
+{"n":"2018","v":"2018"},
+{"n":"2017","v":"2017"},
+{"n":"2016","v":"2016"},
+{"n":"2015","v":"2015"},
+{"n":"2014","v":"2014"},
+{"n":"2013","v":"2013"},
+{"n":"2012","v":"2012"},
+{"n":"2011","v":"2011"},
+{"n":"2010","v":"2010"}
+]
+},
+{
+"name":"地区",
+"key":"area",
+"value":[
+{"n":"全部","v":""},
+{"n":"大陆","v":"大陆"},
+{"n":"欧美","v":"欧美"},
+{"n":"日本","v":"日本"},
+{"n":"其他","v":"其他"}
+]
+},
+{
+"name":"排序",
+"key":"by",
+"value":[
+{"n":"全部","v":""},
+{"n":"时间","v":"time"},
+{"n":"人气","v":"hits"},
+{"n":"评分","v":"score"}
+]
+}
+]
+}
 }
